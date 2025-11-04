b34263cfcf6e072113c2f8df3b21d8c749a3c20e
eslzy <eliot.nouwen@gmail.com>
2025-11-04 23:28:14 +0100

    final section for both opt types, cml portfolio

--- OneDrive/Desktop/travail/UCL/ALGO/portfolio_opt_app/portfolio_optimiser.py
+++ OneDrive/Desktop/travail/UCL/ALGO/portfolio_opt_app/portfolio_optimiser.py
@@ -5,41 +5,41 @@ import yfinance as yf
 import datetime as dt
 from scipy.optimize import minimize
 
-st.title("Simple Portfolio Optimizer")
+st.title("Portfolio Optimizer")
 
 enddate = dt.datetime.now()
-years = st.number_input("years analyzed:", min_value=1, value=5)
+years = st.number_input("years analyzed:", min_value=1, value=5) #its buggy af and usually returns a yfrate limit reached err when you select 5 years 
 startdate = enddate - dt.timedelta(days=365 * years)
 
 
 #stock inputs
 if 'tickers' not in st.session_state:
-    st.session_state['tickers'] = []
+    st.session_state['tickers'] = [] 
 
 new_ticker = st.text_input("Enter a ticker").upper()
-
+                                                            #st.session_state is super useful, it keeps teh inputed data and prevents it from being resetted everytime you select other options, first time using it, its pretty good
 if st.button("Add stock") and new_ticker:
     if new_ticker not in st.session_state['tickers']:
         st.session_state['tickers'].append(new_ticker)
     else:
-        st.warning(new_ticker + " is already in the portfolio.")
+        st.warning("already in the portfolio.")
 
 st.write("Portfolio:")
 st.write(st.session_state['tickers'])
 
-rem = st.selectbox("Remove a stock", [""] + st.session_state['tickers'])
+rem = st.selectbox("Remove a stock from portfolio", [""] + st.session_state['tickers'])
 if st.button("Remove") and rem:
     st.session_state['tickers'].remove(rem)
 
 
 #rfr selection
-region = st.selectbox("Select Risk-Free Rate Region", ["US", "Europe", "Custom rate"])
+region = st.selectbox("Select Risk-Free Rate Region", ["US", "Europe", "Custom rate"]) #idk fi this is worth it especially for multi currency portfolios idk how well handle that 
 
 if region == "US":
-    rf_data = yf.download("^IRX", period="1mo")["Close"]
-    rf = float(rf_data.dropna().iloc[-1]) / 100  # last close, convert % to decimal
+    rf_data = yf.download("^IRX", period="1mo")["Close"] # this is the best ticker i could find for the us rfr, might change later
+    rf = float(rf_data.dropna().iloc[-1]) / 100  # had to convert to float or else it would return a dataframe and would lead to the sharpe ratio also being a dataframe
 elif region == "Europe":
-    rf_data = yf.download("^IRC", period="1mo")["Close"]  # Euro area long-term rate
+    rf_data = yf.download("^IRC", period="1mo")["Close"]  # European long-term rate
     rf = float(rf_data.dropna().iloc[-1]) / 100
 elif region == "Custom rate":
     rf = st.number_input("Enter custom annual risk-free rate (in decimal)", 0.02)
@@ -48,7 +48,8 @@ else:
     
 
 
-#option selection
+#option selection, i want to be able to optimise for sharpe, for risk preferenc and for both. returning respectively the tangency portfolio, a portfolio on the efficient frontier and portfolios on the capital market line
+#i'll handle them all separately, tried doing it all at once and it was super error prone
 
 opt_styles = st.multiselect("Select optimization methods:", ["Maximize Sharpe Ratio", "Optimize for Risk Preference"], default=["Maximize Sharpe Ratio"])
 if not len(st.session_state['tickers'])== 0:
@@ -152,8 +153,44 @@ elif "Optimize for Risk Preference" in opt_styles and "Maximize Sharpe Ratio" no
     # Optional: show numeric table too
     st.dataframe(weights_df.style.format({"Weight": "{:.2%}"}))
     
+#sometimes this is calculated with the utility fuction so maybe ill see if its better to do tah tinstead of max teh return under the volatility constraint
 
-#--------------------------------------------------------------------------------------------------------------------------------------------------------------
+#-------sharpe and risk pref opt (CML)-------------------------------------------------------------------------------------------------------------------------------------------------------
+
+elif "Optimize for Risk Preference" in opt_styles and "Maximize Sharpe Ratio" in opt_styles:
+    
+    constraints = {'type': 'eq', 'fun': lambda w: np.sum(w) - 1}
+    res = minimize(neg_sharpe, start, args=(mean_returns, covariance, rf), method='SLSQP', bounds=bounds, constraints=constraints)
+    
+    opt_weights_tan = res.x         #here we calculate the tangency portfolio (same as teh max_sharpe one) we'll consider this portfolio as one asset
+    (port_return_tan, port_vol_tan) = portfolio_perf(opt_weights_tan, mean_returns, covariance)
+    
+    target_risk = st.slider("Select target volatility (risk %):", min_value=0.0, max_value=0.6, value=0.15, step=0.01)
+    
+    w_t = target_risk / port_vol_tan
+    w_rf = 1 - w_t
+    
+    port_return = rf*(1-w_t) + port_return_tan*w_t
+    port_vol = target_risk    #here we assume teh rf asset is truly risk free but close enough for us to say this 
+    sharpe = (port_return - rf) / port_vol
+    
+    final_weights = opt_weights_tan * w_t
+    
+    st.subheader("Capital Market Line Optimal Portfolio (Combined Risk-Free + Tangency Portfolio)")
+    st.write("Expected Annual Return:", port_return)
+    st.write("Target Volatility:", port_vol)
+    st.write("Sharpe Ratio:", sharpe)
+    st.write("Risk-Free Rate:", rf)
+    st.write("Weight in Tangency Portfolio:", w_t)
+    st.write("Weight in Risk-Free Asset:", w_rf)
+
+
+    weights_df = pd.DataFrame({
+        'Ticker': tickers,
+        'Weight (in total portfolio)': final_weights
+    })
+    st.bar_chart(weights_df.set_index('Ticker'))
+    st.dataframe(weights_df.style.format({"Weight (in total portfolio)": "{:.2%}"}))
 
 
 else:
