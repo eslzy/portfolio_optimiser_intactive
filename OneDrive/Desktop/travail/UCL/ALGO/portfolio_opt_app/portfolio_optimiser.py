97513730d98d02e0797432d03a9276caff7dd50c
eslzy <eliot.nouwen@gmail.com>
2025-11-04 22:20:14 +0100

    added entire section for the risk optimisation selection

--- OneDrive/Desktop/travail/UCL/ALGO/portfolio_opt_app/portfolio_optimiser.py
+++ OneDrive/Desktop/travail/UCL/ALGO/portfolio_opt_app/portfolio_optimiser.py
@@ -40,7 +40,7 @@ if region == "US":
     rf = float(rf_data.dropna().iloc[-1]) / 100  # last close, convert % to decimal
 elif region == "Europe":
     rf_data = yf.download("^IRC", period="1mo")["Close"]  # Euro area long-term rate
-    rf = rf_data.dropna().iloc[-1] / 100
+    rf = float(rf_data.dropna().iloc[-1]) / 100
 elif region == "Custom rate":
     rf = st.number_input("Enter custom annual risk-free rate (in decimal)", 0.02)
 else:
@@ -86,14 +86,15 @@ if allow_shorting:
     bounds = tuple((-1, 1) for _ in range(num_assets))
 else:
     bounds = tuple((0, 1) for _ in range(num_assets))
-constraints = {'type': 'eq', 'fun': lambda w: np.sum(w) - 1}
 start = num_assets * [1.0 / num_assets]
 
 
+#------max sharpe opt--------------------------------------------------------------------------------------------------------------------------------------------------------
 
 if "Maximize Sharpe Ratio" in opt_styles and "Optimize for Risk Preference" not in opt_styles:
-
+    
     #Optimize (Sharpe ratio)
+    constraints = {'type': 'eq', 'fun': lambda w: np.sum(w) - 1} #in section bc we have diff constraints for other selections
     res = minimize(neg_sharpe, start, args=(mean_returns, covariance, rf), method='SLSQP', bounds=bounds,constraints=constraints)
 
     opt_weights = res.x
@@ -114,7 +115,45 @@ if "Maximize Sharpe Ratio" in opt_styles and "Optimize for Risk Preference" not 
     # Optional: show numeric table too
     st.dataframe(weights_df.style.format({"Weight": "{:.2%}"}))#pres techo 5nov 16:15 sale espa tu connais
 
+#-------risk pref opt-------------------------------------------------------------------------------------------------------------------------------------------------------
+elif "Optimize for Risk Preference" in opt_styles and "Maximize Sharpe Ratio" not in opt_styles:
+    
+    def portfolio_vol(weights, cov):
+        return np.sqrt(np.dot(weights, np.dot(cov, weights)))
+                                                                  #redid the f() bc the otehr one handles both erm and vol but we want to max only the return f() so redid one here 
+    def neg_return(weights, returns):
+        return -np.dot(weights, returns)
+    
+    target_risk = st.slider("Select target volatility (risk %):", min_value=0.01, max_value=0.50, value=0.15, step=0.01)
+    
+    constraints = [{'type': 'eq', 'fun': lambda w: np.sum(w) - 1}, {'type': 'eq', 'fun': lambda w: portfolio_vol(w, covariance) - target_risk}] #to make sure the portfolio has the desirred volatility
+    
+    res = minimize(neg_return, start, args=(mean_returns,), method='SLSQP', bounds=bounds, constraints=constraints)
+    
+    opt_weights = res.x
+    port_return, port_vol = portfolio_perf(opt_weights, mean_returns, covariance)
+    sharpe = (port_return - rf) / port_vol
+    
+    #results
+    st.subheader("Risk Optimised Portfolio (Efficient Frontier)")
+    st.write("Expected Annual Return:", port_return)
+    st.write("Desired annual volatility:", target_risk)
+    st.write("Annual Volatility (Risk):", port_vol)
+    st.write("Sharpe Ratio:", sharpe)
+    st.write("Risk Free Rate:",rf)
+
+    # Create and display a bar chart of weights
+    weights_df = pd.DataFrame({'Ticker': tickers,'Weight': opt_weights})
+    st.bar_chart(weights_df.set_index('Ticker'))
+
+    # Optional: show numeric table too
+    st.dataframe(weights_df.style.format({"Weight": "{:.2%}"}))
+    
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
 else:
-    st.write("beebadobee")
+    st.write("Select optimisation type")
 
 
